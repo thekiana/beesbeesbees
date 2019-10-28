d610883df6e8cb73938827e008932b6bdca9d2ac
KKD <keeyanakellydonaldson@Kianas-MacBook-Pro.local>
2019-10-28 12:13:24 -0700

    Implement functionality for Retired Forager Bee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,23 @@
-var RetiredForagerBee = function() {
+var RetiredForagerBee = function(value) {
+  ForagerBee.call(this, value);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = [];
 };
 
+RetiredForagerBee.prototype.eat = function(value) {
+
+};
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+}
+
+RetiredForagerBee.prototype(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
